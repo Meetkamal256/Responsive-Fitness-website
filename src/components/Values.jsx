9cd65aa7f0d645c404259f178965d0902b27dbf9
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-12 13:52:12 +0100

    used map method to fetch data from faqs array

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -25,7 +25,7 @@ const Values = () => {
           <div className="values__wrapper">
             {values.map(({ id, icon, title, desc }) => {
               return (
-                <Card className="values__value">
+                <Card className="values__value" key={id}>
                   <span>{icon}</span>
                   <h4>{title}</h4>
                   <small>{desc}</small>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import FAQs from "../../components/FAQs";
 import MainHeader from "../../components/MainHeader";
 import Programs from "../../components/Programs";
 import Values from "../../components/Values";
@@ -10,6 +11,7 @@ const Home = () => {
       <MainHeader />
       <Programs />
       <Values />
+      <FAQs />
     </>
   );
 };
