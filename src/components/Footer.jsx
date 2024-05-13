8bc8bcbe6321d18da19852c89359a89294def648
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-13 03:45:26 +0100

    footer component jsx done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,12 +14,60 @@ const Footer = () => {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda.
           </p>
           <div className="footer__socials">
-               <a href="https://linkedin.com/" target="_blank" rel="noreferrer noopener"><FaLinkedin /></a>
-               <a href="https://linkedin.com/" target="_blank" rel="noreferrer noopener"><FaFacebook /></a>
-               <a href="https://linkedin.com/" target="_blank" rel="noreferrer noopener"><FaTwitter /></a>
-               <a href="https://linkedin.com/" target="_blank" rel="noreferrer noopener"><FaInstagram /></a>
+            <a
+              href="https://linkedin.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://linkedin.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href="https://linkedin.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://linkedin.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaInstagram />
+            </a>
           </div>
         </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/about">About</Link>
+          <Link to="/plans">Plans</Link>
+          <Link to="/trainers">Trainers</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/contact">Contact</Link>
+        </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/s">Blog</Link>
+          <Link to="/s">Case Studies</Link>
+          <Link to="/s">Events</Link>
+          <Link to="/s">Communities</Link>
+          <Link to="/s">FAQs</Link>
+        </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/contact">Contact Us</Link>
+          <Link to="/s">Support</Link>
+        </article>
+      </div>
+      <div className="footer__copyright">
+        <small>2024 &copy; </small>
       </div>
     </footer>
   );
