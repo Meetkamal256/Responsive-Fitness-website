788192f3d492ca13bdd0c50862b7390e3f46720e
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-15 00:54:15 +0100

    NotFound page jsx and styling done

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,9 +1,17 @@
-import './notFound.css';
+import { Link } from "react-router-dom";
+import "./notFound.css";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <section>
+      <div className="container notFound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className="btn">
+          Go back Home
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default NotFound
+export default NotFound;
