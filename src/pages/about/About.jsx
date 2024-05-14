2c1d1f605a4ffcf54162e79bc6671fba73cea7c8
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-14 22:53:22 +0100

    contact component jsx and styling done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,8 @@
 import Header from "../../components/Header";
 import HeaderImage from "../../images/header_bg_1.jpg";
-import StoryImage from "../../images/about1.jpg";
-import VisionImage from "../../images/about2.jpg";
-import MissionImage from "../../images/about3.jpg";
+import StoryImage from "../../images/about7.jpg";
+import VisionImage from "../../images/about4.jpg";
+import MissionImage from "../../images/about6.jpg";
 import "./about.css";
 
 const About = () => {
